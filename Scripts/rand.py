76e5ccc93e0f39a6d2c996f6af7087728cfdd821
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-05-22 10:19:46 +0200

    Seeded number generator

---
+++ Scripts/rand.py
@@ -0,0 +1,10 @@
+import random
+import sys
+seed = sys.argv[4] if len(sys.argv) > 4 else 22052020
+random.seed(seed)
+up = int(sys.argv[3])
+low = int(sys.argv[2])
+num = int(sys.argv[1])
+r = [random.randint(low, up) for i in range(num)]
+
+print(r)
