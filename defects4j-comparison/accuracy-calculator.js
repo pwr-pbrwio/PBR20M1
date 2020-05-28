2ed987de07251182dc403717088af884b619c95f
Patryk Kramarczyk <pkramarczyk16@gmail.com>
2020-05-28 20:12:42 +0200

    Defects4j comaprison script

---
+++ defects4j-comparison/accuracy-calculator.js
@@ -0,0 +1,85 @@
+const fse = require("fs-extra");
+const csv = require("csvtojson");
+
+const PROJECT_NAME = "commons-math";
+const DATA_INPUT_FILE = "./data/input/commons-math-v2-d2.json";
+const DEFECTS4J_INPUT_FILE = "./bugs_dataset.csv";
+
+const main = async () => {
+  const [szzResults, bugDb] = await Promise.all([
+    getSzzResults(DATA_INPUT_FILE),
+    getDefects4jData(DEFECTS4J_INPUT_FILE),
+  ]);
+
+  const uniqueSzzResults = filterDuplicatedPairs(szzResults);
+
+  const matchingPairs = getMatchingPairs(uniqueSzzResults, bugDb);
+  const accuracy = calculateAccuracy(
+    matchingPairs.length,
+    uniqueSzzResults.length
+  );
+  console.log({ accuracy });
+};
+
+const calculateAccuracy = (matchedCasesCount, allCasesCount) =>
+  matchedCasesCount / allCasesCount;
+
+const filterBugsForProject = (bugDatabase, projectName) =>
+  bugDatabase.filter((record) => record.project === projectName);
+
+const getMatchingPairs = (szzResults, bugDatabase) =>
+  bugDatabase.filter((record) =>
+    szzResults.some(
+      (result) =>
+        record.bugintroducingchange_commit === result.bugIntroducerCommitId &&
+        record.bugfix_commit === result.bugFixCommitId
+    )
+  );
+
+const filterDuplicatedPairs = (pairs) => {
+  const uniquePairsMap = new Map();
+  pairs.forEach((pair) =>
+    uniquePairsMap.set(pair.bugFixingId + pair.bugInducingId, {
+      ...pair,
+    })
+  );
+
+  return Array.from(uniquePairsMap.values());
+};
+
+const getSzzResults = async (path) => {
+  const inputData = await loadJSONFile(path);
+  return convertArraysArrayToObjectsArray(inputData);
+};
+
+const getDefects4jData = (csvFilePath) => {
+  return csv().fromFile(csvFilePath);
+};
+
+const convertArraysArrayToObjectsArray = (data) => {
+  return data.map((pair) => {
+    return {
+      bugFixCommitId: pair[0],
+      bugIntroducerCommitId: pair[1],
+      path: pair[2],
+    };
+  });
+};
+
+const loadJSONFile = (filepath) => {
+  return new Promise((resolve, reject) => {
+    fse.readFile(filepath, "utf8", (err, content) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          resolve(JSON.parse(content));
+        } catch (err) {
+          reject(err);
+        }
+      }
+    });
+  });
+};
+
+main();
