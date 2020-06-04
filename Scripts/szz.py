bac22c0b46d05c59801731d025abf6e621007532
Patryk Kramarczyk <pkramarczyk16@gmail.com>
2020-06-04 21:14:03 +0200

    Added alternative way to fetch issues from github

--- Scripts/szz.py
+++ Scripts/szz.py
@@ -2,7 +2,8 @@ import os
 import argparse
 import json
 
-from unleashed.fetch import fetch
+from unleashed.fetch import fetch as jira_issue_fetcher
+from github_issue_client import fetch as github_issue_fetcher
 from unleashed.git_log_to_array import git_log_to_json
 from unleashed.find_bug_fixes import find_bug_fixes
 
@@ -25,6 +26,8 @@ if __name__ == "__main__":
                         help="Jira tag.")
     parser.add_argument('--depth', type=str,
                         help="Szz depth.")
+    parser.add_argument('--fetchStrategy', type=str,
+                        help="Issues fetch strategy.")
 
     try:
         args = parser.parse_args()
@@ -34,21 +37,23 @@ if __name__ == "__main__":
         owner = args.owner
         tag = args.tag
         depth = 3 if args.depth == None else args.depth
+        issueFetchingStrategy = 'jira' if args.fetchStrategy == None else args.fetchStrategy
 
         os.makedirs('.temp', exist_ok=True)
-        fetch(tag, jira, '.temp/fetch_issues')
+        if issueFetchingStrategy == 'github':
+            github_issue_fetcher(owner, repo, '.temp/fetch_issues')
+        elif issueFetchingStrategy == 'jira':
+            jira_issue_fetcher(tag, jira, '.temp/fetch_issues')
+        else:
+            raise Exception('Invalid fetch strategy!')
 
-        sha = getFirstSha(owner, repo)
-        git_log_to_json(sha, repoPath, '.temp')
+        # sha = getFirstSha(owner, repo)
+        # git_log_to_json(sha, repoPath, '.temp')
         issue_list = find_bug_fixes('.temp/fetch_issues', '.temp/gitlog.json',
                                     r'{tag}-{{nbr}}\D|#{{nbr}}\D'.format(tag=tag.upper()))
 
         with open('.temp/issue_list.json', 'w', encoding="utf8") as f:
             f.write(json.dumps(issue_list))
-
-        print('Run unleashed szz...')
-        os.system(commandFormat.format(file=os.path.dirname(
-            os.path.realpath(__file__)), repoPath=repoPath, depth=depth))
 
     except Exception as e:
         print('\u001b[31m{} failed with {}!!!\u001b[0m'.format(repo, e))
