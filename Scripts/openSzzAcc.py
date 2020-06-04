8371b04e2528388de518b30982012aaa6c12f3c4
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-06-04 21:20:36 +0200

    Measure openszz accuracy

---
+++ Scripts/openSzzAcc.py
@@ -0,0 +1,106 @@
+from toolz.curried import pipe, map, filter, get
+import os
+import csv
+import argparse
+
+netoPath = os.path.dirname(__file__) + '/git_neto.csv'
+openPath = './BugInducingCommits.csv'
+
+
+def main(repoName):
+
+    openData = []
+    with open(openPath, newline='', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        readerList = list(reader)
+
+        labels = None
+        for rowId in range(1):
+            labels = readerList[rowId]
+        for rowId in range(1, len(readerList)):
+            z = zip(labels, readerList[rowId])
+            if len(readerList[rowId]) > 1:
+                d = dict(z)
+                openData.append(d)
+
+    # remove repetitions
+    openData = [(x['bugFixingId'], x['bugInducingId'],
+                 x['bugFixingfileChanged']) for x in openData]
+    openData = list(dict.fromkeys(openData))
+
+    # Read csv file into 'netoData' variable
+    netoData = []
+    with open(netoPath, newline='', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        readerList = list(reader)
+
+        labels = None
+        for rowId in range(1):
+            labels = readerList[rowId]
+        for rowId in range(1, len(readerList)):
+            z = zip(labels, readerList[rowId])
+            d = dict(z)
+            if d['project'] == repoName:
+                netoData.append(d)
+
+    # remove repetitions
+    netoData = [(x['bugfix_commit'], x['bugintroducingchange_commit'],
+                 x['pathbugfix']) for x in netoData]
+    netoData = list(dict.fromkeys(netoData))
+
+    notMatched = netoData.copy()
+    notMatchedWithFile = netoData.copy()
+    notMatchedFixes = netoData.copy()
+
+    # Iterate over szz results
+    for unl in openData:
+        notMatchedFixes = pipe(
+            notMatchedFixes, filter(lambda x: not (x[0] == unl[0])), list)
+
+        notMatched = pipe(
+            notMatched, filter(lambda x: not (x[0] == unl[0] and x[1] == unl[1])), list)
+
+        if len(unl) > 2:
+            notMatchedWithFile = pipe(
+                notMatchedWithFile, filter(lambda x: not (x[0] == unl[0] and x[1] == unl[1] and x[2].endswith(unl[2]))), list)
+
+    acc = (len(netoData) - len(notMatched)) / len(netoData)
+    accWFile = (len(netoData) - len(notMatchedWithFile)) / len(netoData)
+    accFixes = (len(netoData) - len(notMatchedFixes)) / len(netoData)
+
+    print(f'fixes matched: {accFixes}')
+    print(f'acc: {acc}')
+    print(f'acc with file: {accWFile}')
+
+    # Test how many matches in ra-unleashed are correct
+    notMatched = openData.copy()
+    notMatchedWithFile = openData.copy()
+
+    for res in netoData:
+        notMatched = pipe(
+            notMatched, filter(lambda x: not (x[0] == res[0] and x[1] == res[1])), list)
+
+        if len(openData[0]) > 2:
+            notMatchedWithFile = pipe(
+                notMatchedWithFile, filter(lambda x: not (x[0] == res[0] and x[1] == res[1] and res[2].endswith(x[2]))), list)
+
+    acc = (len(openData) - len(notMatched)) / len(openData)
+    accWFile = (len(openData) - len(notMatchedWithFile)) / len(openData)
+
+    print()
+    print('Test how many in openSzz are matched')
+    print(f'acc: {acc}')
+    print(f'acc with file: {accWFile}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="""Convert a git log output to json.""")
+    parser.add_argument('--repoName', type=str,
+                        help="Repository name.")
+
+    args = parser.parse_args()
+    if args.repoName == None:
+        print('Usage: python script.py --repoName=name')
+        exit(1)
+    main(args.repoName)
